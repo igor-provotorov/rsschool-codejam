9282f437ffe1b6c1f70da3b0ca03bde68bb4f36d
igor-provotorov <garynya@yandex.ru>
2018-10-28 12:55:04 +0300

    feat: implement function 'sumOfOther'

---
+++ src/sumOfOther.js
@@ -0,0 +1,18 @@
+const sumOfOther = (array) => {
+  if (array.length < 2) {
+    return array;
+  } else if (array.length === 2) {
+    return array.replace();
+  }
+
+  let sum = 0;
+  for (let i = 0, len = array.length; i < len; i++) {
+    sum += array[i];
+  }
+
+  for (let i = 0, len = array.length; i < len; i++) {
+    array[i] = sum - array[i];
+  }
+
+  return array;
+}
