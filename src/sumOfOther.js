2def3691ef0539faeca83668285b3c5a19c47713
igor-provotorov <garynya@yandex.ru>
2018-10-28 13:28:58 +0300

    fix: Array.replase() to Array.reverse() bug fix

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -2,8 +2,9 @@ const sumOfOther = (array) => {
   if (array.length < 2) {
     return array;
   } else if (array.length === 2) {
-    return array.replace();
+    return array.reverse();
   }
+
 
   let sum = 0;
   for (let i = 0, len = array.length; i < len; i++) {
