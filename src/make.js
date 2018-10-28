2ac221f37675d10413fa12f1a4f1b9d718a85eab
igor-provotorov <garynya@yandex.ru>
2018-10-28 17:26:19 +0300

    feat: implement function#2 'make'

--- src/make.js
+++ src/make.js
@@ -1,1 +1,17 @@
+const make = (...args) => {
+  const arr = [];
+  arr.push(...args);
 
+  const closureFunction = (...args2) => {    
+    if (typeof args2[0] === 'function') {
+      const foo = args2[0];
+      return arr.reduce((acc, item) => foo(acc, item));
+    }
+    arr.push(...args2);
+    return closureFunction;
+  };
+
+  return closureFunction;
+}
+
+module.exports = make;
